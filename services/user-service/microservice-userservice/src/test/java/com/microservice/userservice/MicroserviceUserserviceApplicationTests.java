3b67198a5f73ff089ed7f1d5b74a25d09400d963
RicoLancheros <rico.lancheros@hotmail.com>
2025-01-30 14:10:17 -0500

    V0.0.1.2: User-Service y MYSQL en docker
    Se crea user-service, se configura Mysql en docker, se configura el archivo application properties.

---
+++ services/user-service/microservice-userservice/src/test/java/com/microservice/userservice/MicroserviceUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
