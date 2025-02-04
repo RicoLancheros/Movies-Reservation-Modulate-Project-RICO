4b29a7036332fb683ad6224bdc4ab069b82b006e
RicoLancheros <rico.lancheros@hotmail.com>
2025-02-03 22:03:45 -0500

    V0.0.1.5: Se corrigue la clase User
    Se trabaja en la clase User, se configura y se adapta.

--- services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/entity/user.java
+++ services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/entity/user.java
@@ -6,23 +6,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users") // Especifica el nombre de la tabla en la base de datos
-@Data // Lombok: genera getters, setters, equals, hashCode, toString
-@NoArgsConstructor // Lombok: genera constructor sin argumentos
-@AllArgsConstructor // Lombok: genera constructor con todos los argumentos
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class user {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autoincremento en MySQL
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true) // No puede ser nulo y debe ser único
+    @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false) // No puede ser nulo
+    @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false) // No puede ser nulo
+    @Column(nullable = false)
     private String email;
 
     // Puedes añadir más campos como firstName, lastName, etc., según tu esquema de base de datos
