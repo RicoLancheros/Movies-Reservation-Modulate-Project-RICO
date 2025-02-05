48ee3cd8d7f35764c0923dc0dda3093c6ee76637
RicoLancheros <rico.lancheros@hotmail.com>
2025-02-04 23:47:52 -0500

    V0.0.1.6: Se configura Aplication Properties
    Se configura app properties y se comienza con el apartado de los controlladores.

--- services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/repository/UserRepository.java
+++ services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.microservice.userservice.repository;
 
-
 import com.microservice.userservice.entity.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
