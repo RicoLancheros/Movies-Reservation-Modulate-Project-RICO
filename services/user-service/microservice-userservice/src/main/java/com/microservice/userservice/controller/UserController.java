8527b701b4960d4d53e07dd99d9b9632074c60bc
RicoLancheros <rico.lancheros@hotmail.com>
2025-02-05 20:32:00 -0500

    V0.0.1.7: Controlladores y PRIMER IMPEDIMENTO:
    Se crea el UserController, y se intenta un Clean and install de Maven, no funciona porque no se logra conectar a la base de datos, el problema parece radicar en el POM.XML o en la misma conexion a docker, revisar los 2 archivos en la vercion 0.0.1.8

---
+++ services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.microservice.userservice.controller;
+
+import com.microservice.userservice.entity.User;
+import com.microservice.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
--- services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/repository/UserRepository.java
+++ services/user-service/microservice-userservice/src/main/java/com/microservice/userservice/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.microservice.userservice.repository;
 
-import com.microservice.userservice.entity.user;
+import com.microservice.userservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<user, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
